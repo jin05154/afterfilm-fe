b74c85666586472a5706c8db9350bc5ecc36f636
Yumin Lee <yuminlee@Yumins-MacBook-Air.local>
2024-05-04 16:41:03 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          애프터필름
         </p>
         <a
           className="App-link"
@@ -15,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World!
         </a>
       </header>
     </div>
