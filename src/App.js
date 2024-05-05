b59a848e2859a2359eb574ecfac715bff04c715d
kimminji <minji5276@gmail.com>
2024-05-05 20:50:45 +0900

    header 수정

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
         <Header />
-        <MainPage />
+          <MainPage />
       </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,20 @@
-
+import { VscSearch } from "react-icons/vsc";
 
 const Header = () => {
     return (
-        <header>
+        <div className="headerLayout">
             <a 
-            className="header"
+            className="logo"
             href="/"
             target="_blank"
             rel="noopener noreferrer"
             >애프터 필름</a>
-            <nav>
-                <input type="text"/>
-            </nav>
-        </header>
+            <label className="searchBox">
+                <VscSearch className="searchIcon"/>
+                <input className="searchInput" type="text" placeholder="영화, 배우, 게시물을 검색해보세요."/>
+            </label>
+            <button className="signButton">Sign In</button>
+        </div>
     )
 }
 export default  Header
