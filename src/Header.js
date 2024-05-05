2bfd79de307bb5bd77436be4759251f17e83aa18
kimminji <minji5276@gmail.com>
2024-05-05 21:44:14 +0900

    header 수정

--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
             href="/"
             target="_blank"
             rel="noopener noreferrer"
-            >애프터 필름</a>
+            >애프터필름</a>
             <label className="searchBox">
                 <VscSearch className="searchIcon"/>
                 <input className="searchInput" type="text" placeholder="영화, 배우, 게시물을 검색해보세요."/>
