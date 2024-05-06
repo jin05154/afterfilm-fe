74f7b0846ddeb764c8aee7632757156d724110fd
Yumin Lee <dorayuminlee@gmail.com>
2024-05-07 00:02:45 +0900

    Move: mock data 파일 별도 생성

--- src/components/Common/CardGroup.js
+++ src/components/Common/CardGroup.js
@@ -1,26 +1,6 @@
 import Card from "./Card";
 
-/* Sample data */
-const MOVIEDATA = [
-  {
-    id: 1,
-    title: "Air Doll",
-    desc: "A life-size blow-up doll comes to life and falls in love with a video store clerk in Kore-eda Hirokazu’s haunting, melancholy meditation on urban alienation.",
-    img: "https://player.bfi.org.uk/sites/default/files/styles/landscape_320x180/public/hero-images/Air-Doll-02.jpg?itok=f7OVtxqK",
-  },
-  {
-    id: 2,
-    title: "Bande à part",
-    desc: "Godard's playful tribute to the Hollywood pulp crime movies of the 1940s, executed with typically Gallic cool.",
-    img: "https://player.bfi.org.uk/sites/default/files/styles/landscape_320x180/public/hero-images/20160116-bande-a-parte-qa-with-anna-karina-1920x1080.jpg?itok=KyN-x9ff",
-  },
-  {
-    id: 3,
-    title: "Wild Men",
-    desc: "In this razor-sharp black comedy featuring Sofie Gråbøl (The Killing), Martin flees to the mountains in a desperate attempt to cure his midlife crisis.",
-    img: "https://player.bfi.org.uk/sites/default/files/styles/landscape_640x360/public/hero-images/Wild_Men_02-1920.jpg?itok=SkqdIu5l",
-  },
-];
+import MOVIEDATA from "../../assets/mockData";
 
 function CardGroup() {
   return (
