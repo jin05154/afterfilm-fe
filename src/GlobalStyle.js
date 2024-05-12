0342e78a1a2b79d413ed2ba9899e5d6de92d6a09
yuminlee <dorayuminlee@gmail.com>
2024-05-12 23:51:35 +0900

    design: font-face 정의

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -2,14 +2,18 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   * {
-    @import url("https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_JAMO@1.0/Diphylleia-Regular.woff2");
+    @font-face {
+      font-family: 'Diphylleia-Regular';
+      src: url("https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_JAMO@1.0/Diphylleia-Regular.woff2");
+    }
     font-family: Diphylleia-Regular, sans-serif;
-    font-weight: bold;
+    font-weight: normal;
   }
   .App {
     height: 100vh;
     width: 100vw;
     min-width: 1200px;
+    overflow: hidden;
 
     a {
       text-decoration: none;
--- src/components/Common/Body.js
+++ src/components/Common/Body.js
@@ -7,7 +7,7 @@ const SBodyWrapper = styled.div`
   height: calc(100vh - (50px + 158px) - 20px);
   background-color: ${COLOR.themeBlack};
   padding: 10px 20px;
-  overflow: scroll;
+  overflow: hidden;
 `;
 
 function Body({ children }) {
