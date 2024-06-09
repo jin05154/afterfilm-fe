6b211663a0ca7e370eaf3a2f1b150d38dfcf49ec
yuminlee <dorayuminlee@gmail.com>
2024-06-09 23:36:46 +0900

    feat: 상세페이지 Film Header 포스터, 스틸컷, ott 바로가기 추가

--- src/utils/color.js
+++ src/utils/color.js
@@ -4,8 +4,11 @@ const COLOR = {
   themeWhite: "#F8F8F8",
   themeYellow: "#F7C873",
   themeBeige: "#FAEBCD",
-
+  
+  /* 추가 색상 */
+  themeGray: "#5A5A5A",
   semiTransparentBackground: "rgba(0, 0, 0, 0.5)",
+  gradientBackground: "rgba(0, 0, 0, 0.7)",
 };
 
 export default COLOR;
