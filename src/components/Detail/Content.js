20021c56dc3a205196b8421f4ab6fe5e27922040
yuminlee <dorayuminlee@gmail.com>
2024-06-09 20:16:47 +0900

    style: 코드 포맷 수정

--- src/components/Detail/Content.js
+++ src/components/Detail/Content.js
@@ -4,17 +4,26 @@ import CardGroup from "../Common/CardGroup";
 import PostGroup from "./PostGroup";
 import Gallery from "./Gallery";
 
+const contentMap = {
+  card: CardGroup,
+  community: PostGroup,
+  gallery: Gallery,
+};
+
 function Content({ children, type, title }) {
+  const ContentType = contentMap[type] || null;
   return (
     <SContentWrapper>
-      <SContentTitle>
-        <span>{title}</span>
-        {/* 더보기 버튼 - 버튼 컴포넌트 생성 필요 */}
-        <p>더보기</p>
-      </SContentTitle>
-      {type === "card" && <CardGroup />}
-      {type === "community" && <PostGroup/>}
-      {type === "gallery" && <Gallery/>}
+      {ContentType && (
+        <>
+          <SContentTitle>
+            <span>{title}</span>
+            {/* 더보기 버튼 - 버튼 컴포넌트 생성 필요 */}
+            <p>더보기</p>
+          </SContentTitle>
+          <ContentType />
+        </>
+      )}
       {children}
     </SContentWrapper>
   );
